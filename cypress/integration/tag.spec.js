1faada4e0b92f5c02403150b2d1aae1ba3f71d38
Brandon <b.gonzalezp@uniandes.edu.co>
2022-05-08 19:57:29 -0500

    fix tag feature cypress

--- cypress/integration/tag.spec.js
+++ cypress/integration/tag.spec.js
@@ -24,18 +24,21 @@ describe('Tag Flow', () => {
   }
 
   function createNewTag(){ 
-    cy.visit('http://localhost:2368/ghost/#/tags');
-    const button = cy.get('a[href="#/tags/new/"]');
-    button.click();
+    cy.visit('http://localhost:2368/ghost/#/tags');  cy.wait(2000)
+    cy.xpath("//span[normalize-space()='New tag']").click();
+  
+    
     const tagName = cy.get('#tag-name');
     tagName.type(makeDescription(10));
     const saveButton = cy.get('html:nth-of-type(1) > body:nth-of-type(1) > div:nth-of-type(2) > div:nth-of-type(1) > main:nth-of-type(1) > section:nth-of-type(1) > form:nth-of-type(1) > div:nth-of-type(1) > header:nth-of-type(1) > section:nth-of-type(1) > button:nth-of-type(1) > span:nth-of-type(1)');
     saveButton.click()
+    cy.wait(1000)
     cy.visit('http://localhost:2368/ghost/#/tags');
   }
 
   function createNewTagInvalidColor(){ 
     cy.visit('http://localhost:2368/ghost/#/tags');
+    cy.wait(2000)
     const button = cy.get('a[href="#/tags/new/"]');
     button.click();
     const tagName = cy.get('#tag-name');
@@ -46,10 +49,12 @@ describe('Tag Flow', () => {
     saveButton.click();
     const colorError = cy.get('input[placeholder=15171A]')
     colorError.should('have.css', 'border-color', 'rgb(245, 11, 35)')
+    cy.visit("http://localhost:2368/ghost/")
   }
 
   function createNewTagInvalidDescription(){ 
     cy.visit('http://localhost:2368/ghost/#/tags');
+    cy.wait(2000)
     const button = cy.get('a[href="#/tags/new/"]');
     button.click();
     const tagName = cy.get('#tag-name');
@@ -59,19 +64,23 @@ describe('Tag Flow', () => {
     saveButton.click()
     const tagDescriptionWithError = cy.get('#tag-description');
     tagDescriptionWithError.should('have.css', 'border-color', 'rgb(245, 11, 35)')
+    cy.visit("http://localhost:2368/ghost/")
   }
 
   function createInternalTag() {
     cy.visit('http://localhost:2368/ghost/#/tags?type=internal');
-    const button = cy.get('a[href="#/tags/new/"]');
-    button.click();
+    cy.wait(2000)
+    cy.xpath('/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/div[1]/header[1]/section[1]/a[1]/span[1]').click()
+  
+    cy.wait(2000)
     const tagName = cy.get('#tag-name');
     const internalName = 'internal ' + makeDescription(10);
     tagName.type(internalName);
     const saveButton = cy.get('html:nth-of-type(1) > body:nth-of-type(1) > div:nth-of-type(2) > div:nth-of-type(1) > main:nth-of-type(1) > section:nth-of-type(1) > form:nth-of-type(1) > div:nth-of-type(1) > header:nth-of-type(1) > section:nth-of-type(1) > button:nth-of-type(1) > span:nth-of-type(1)');
     saveButton.click()
-    cy.visit('http://localhost:2368/ghost/#/tags');
-    const tagList = cy.get('.tags-list.gh-list').children('li');
+    cy.visit('http://localhost:2368/ghost/#/tags?type=internal');
+    cy.wait(2000)
+  
     
   }
 
