ca9709da6b3d4b95707e1e5f0c3a37a77c6d15dc
Brandon <b.gonzalezp@uniandes.edu.co>
2022-05-13 17:45:32 -0500

    Page feature ghost 3.42

---
+++ cypress/integration/pruebas-de-regresion/3.42/page.spec.js
@@ -0,0 +1,90 @@
+describe("Page Flow", function () {
+    it("Page", function () {
+      cy.visit("http://localhost:3001/ghost/");
+      cy.viewport(1920, 1080)
+      cy.wait(1000);
+     
+      crear_pagina("Crear pagina");
+     
+    });
+  });
+
+  const ruta="/scenario_createPage"
+function logIn(i) {
+ 
+    cy.get('#ember8').type('prueba@example.com', { force: true });
+    cy.get('#ember10').type('prueba@example.com123456789', { force: true });
+    cy.wait(1000);
+    cy.screenshot(ruta+"/step1",{overwrite: true, capture: 'fullPage'})
+    cy.get('#ember12').click({ force: true });
+    cy.wait(5000);
+    cy.screenshot(ruta+"/step"+i,{overwrite: true, capture: 'fullPage'})
+
+
+}
+
+function crear_pagina(titulo) {
+    let i=2;
+    logIn(i.toString());
+    i+=1;
+    // IR a la seccion de pages
+
+    cy.wait(1000);
+    // Click en el boton de page
+    cy.xpath("/html/body/div[2]/div/nav[1]/section/div[1]/ul[2]/li[3]/a").click()
+    cy.wait(2000);
+    cy.screenshot(ruta+"/step"+i,{overwrite: true, capture: 'fullPage'})
+    cy.xpath("/html/body/div[2]/div/main/section/header/section/a/span").click()
+    cy.wait(1000)
+    tituloYDescricpion("Crear pagina")
+    i+=1;
+    publicarPagina(i.toString())
+    i+=1
+    cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+    i+=1
+    cy.xpath("/html/body/div[2]/div/main/section/header/section/div/div[1]/div[1]/span").click()
+    cy.wait(1000)
+     cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+     i+=1
+    cy.xpath("/html/body/div[1]/div/ul/li[3]").click()
+    cy.wait(1000)
+    cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+    verificarTitulo("Crear pagina")
+    
+   
+  }
+
+
+  function verificarTitulo(titulo){
+    cy.xpath("/html/body/div[2]/div/main/section/section/ol/li[2]/a[2]/h3").invoke("text")
+    .then((text) => {
+  
+      if (text.includes(titulo)){ cy.log("Prueba de " + titulo + " Fue exitosa");}
+      else{ cy.log("Prueba de " + titulo + " Fallo");}
+    });
+  }
+  function publicarPagina(i) {
+    // Click en publish
+    cy.xpath("/html/body/div[2]/div/main/section/header/section/div/div[1]/span").click();
+     cy.screenshot(ruta+"/step"+i,{overwrite: true, capture: 'fullPage'})
+    cy.xpath("/html/body/div[1]/div/footer/button[2]/span").click();
+  
+    cy.wait(2000);
+    // Volver a la seccion de pages
+    cy.xpath(
+      "/html/body/div[2]/div/main/section/header/div/div[1]/a"
+    ).click();
+    cy.wait(2000);
+  }
+  function tituloYDescricpion(titulo) {
+    let elementTitulo = cy.xpath(
+      "/html/body/div[2]/div/main/section/div[1]/div[1]/textarea"
+    );
+    elementTitulo.type(titulo);
+    let elementDescription = cy.xpath(
+      "/html/body/div[2]/div/main/section/div[1]/div[1]/article/div[1]/div[1]"
+    );
+  
+    elementDescription.type("Descripcion");
+    cy.wait(1000);
+  }
--- cypress/integration/pruebas-de-regresion/4.47/page.spec.js
+++ cypress/integration/pruebas-de-regresion/4.47/page.spec.js
@@ -1,81 +1,107 @@
 describe("Page Flow", function () {
   it("Page", function () {
     cy.visit("http://localhost:2368/ghost/");
+  
+    cy.viewport(1920, 1080)
     cy.wait(1000);
-    crear_pagina("Crear pagina");
-    crear_pagina_programada("Crear Pagina programada");
-    crear_pagina_con_draft("Crear pagina usando un draft");
-    crear_pagina_con_nombre_extenso("Crear pagina con nombre extenso");
+    crear_pagina("Crear pagina","/scenario_createPage");
+    crear_pagina_programada("Crear Pagina programada","/sceneario_createScheduledPage");
+    crear_pagina_con_draft("Crear pagina usando un draft","/scenario_createPageDraft");
+    crear_pagina_con_nombre_extenso("Crear pagina con nombre extenso","/scenario_longName");
   });
 });
 
-function logIn() {
- 
-    cy.get('#ember8').type('prueba@example.com', { force: true });
-    cy.get('#ember10').type('prueba@example.com123456789', { force: true });
+function logIn(i,ruta) {
+  
+    
+    cy.get("#ember7").type("prueba@example.com", { force: true });
+    cy.get("#ember9").type("prueba@example.com123456789", { force: true });
     cy.wait(1000);
-
-    cy.get('#ember12').click({ force: true });
+    cy.screenshot(ruta+"/step"+i,{overwrite: true, capture: 'fullPage'})
+    cy.get('#ember11').click({ force: true });
     cy.wait(5000);
-    cy.screenshot()
+    
 
 
 }
-function crear_pagina(titulo) {
-  logIn();
-  cy.wait(5000);
+
+function crear_pagina(titulo,ruta) {
+  let i=1;
+  logIn(i.toString(),ruta);
+  i+=1
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  
   // IR a la seccion de pages
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/nav[1]/div[1]/section[1]/div[1]/ul[2]/li[2]/a[1]"
   ).click({ force: true });
-
+ 
   cy.wait(1000);
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
   // Click en el boton de page
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/div[1]/header[1]/section[1]/a[1]/span[1]"
   ).click({ force: true });
-
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1 
   //llenar los campos
   tituloYDescricpion(titulo);
-  publicarPagina();
-
+  publicarPagina(i.toString(),ruta);
+  i+=1
   // Ver las paginas publicadas
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/div[1]/header[1]/section[1]/div[1]/div[1]/div[1]"
   ).click();
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1 
   cy.wait(1000);
   cy.xpath("/html[1]/body[1]/div[1]/div[1]/ul[1]/li[3]").click();
   cy.wait(2000);
-
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1 
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/div[1]/header[1]/section[1]/div[1]/div[2]/div[1]"
   ).click();
   cy.wait(2000);
   cy.xpath("/html[1]/body[1]/div[1]/div[1]/ul[1]/li[1]").click();
-
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1 
   verificarTitulo(titulo);
   cy.wait(1000);
   exit();
 }
 
-function crear_pagina_programada(titulo) {
-  logIn();
+function crear_pagina_programada(titulo,ruta) {
+  let i=1;
+  logIn(i.toString(),ruta);
+  i+=1
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
+  cy.wait(3000);
 
-  cy.wait(5000);
+  // Ir a la seccion de pages
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/nav[1]/div[1]/section[1]/div[1]/ul[2]/li[2]/a[1]"
   ).click({ force: true });
+  
   cy.wait(1000);
+  
   // Click en el boton de page
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/div[1]/header[1]/section[1]/a[1]/span[1]"
   ).click({ force: true });
+
   tituloYDescricpion(titulo);
   cy.wait(2000);
   // Click en publish
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/main[1]/div[1]/section[1]/header[1]/section[1]/div[2]/div[1]/span[1]"
   ).click();
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
   //Click en el radio button de programar
   cy.wait(1000);
   cy.xpath(
@@ -91,6 +117,8 @@ function crear_pagina_programada(titulo) {
     "/html[1]/body[1]/div[2]/div[1]/main[1]/div[1]/section[1]/header[1]/div[1]/div[1]/a[1]/span[1]"
   ).click();
   cy.wait(2000);
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
   // Ver paginas programadas
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/div[1]/header[1]/section[1]/div[1]/div[1]/div[1]"
@@ -98,22 +126,33 @@ function crear_pagina_programada(titulo) {
   cy.wait(1000);
   cy.xpath("/html[1]/body[1]/div[1]/div[1]/ul[1]/li[4]").click();
   cy.wait(2000);
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
   verificarTitulo(titulo);
   cy.wait(1000);
   exit();
 }
 
-function crear_pagina_con_draft(titulo) {
-  logIn();
-  cy.wait(5000);
+function crear_pagina_con_draft(titulo,ruta) {
+  let i=1;
+  logIn(i.toString(),ruta);
+  i+=1
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1; 
+
+  cy.wait(3000);
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/nav[1]/div[1]/section[1]/div[1]/ul[2]/li[2]/a[1]"
   ).click({ force: true });
   cy.wait(1000);
+  
   // Click en el boton de page
+  
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/div[1]/header[1]/section[1]/a[1]/span[1]"
   ).click({ force: true });
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
   tituloYDescricpion(titulo);
   // Volver a la seccion de pages
   cy.xpath(
@@ -126,9 +165,12 @@ function crear_pagina_con_draft(titulo) {
     "/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/div[1]/header[1]/section[1]/div[1]/div[1]/div[1]/span[1]"
   ).click();
   cy.wait(1000);
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
   cy.xpath("/html[1]/body[1]/div[1]/div[1]/ul[1]/li[2]").click();
   cy.wait(2000);
-
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
   verificarTitulo(titulo);
 
   //Editar el draft
@@ -137,17 +179,19 @@ function crear_pagina_con_draft(titulo) {
   ).click();
   cy.wait(1000);
   // Cambiar visibilidad de la pagina
-  changeVisibility();
-
+  changeVisibility(i,ruta);
+  i+=1
   // Click en publish
-  publicarPagina();
-
+  publicarPagina(i.toString(),ruta);
+  i+=1
   cy.wait(2000);
 
   //Ir a la seccion de paginas publicadas
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/div[1]/header[1]/section[1]/div[1]/div[1]/div[1]/span[1]"
   ).click();
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
   cy.wait(2000);
   cy.xpath("/html[1]/body[1]/div[1]/div[1]/ul[1]/li[3]").click();
   cy.wait(2000);
@@ -158,42 +202,60 @@ function crear_pagina_con_draft(titulo) {
   cy.wait(2000);
   cy.xpath("/html[1]/body[1]/div[1]/div[1]/ul[1]/li[4]").click();
   cy.wait(2000);
-
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
   verificarTitulo(titulo);
   cy.wait(1000);
   exit();
 }
 
-function changeVisibility() {
+function changeVisibility(i,ruta) {
   cy.xpath("/html[1]/body[1]/div[2]/div[1]/main[1]/button[1]/span[1]").click();
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+
   cy.wait(1000);
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/main[1]/div[1]/div[1]/div[1]/div[1]/div[2]/form[1]/div[4]/div[1]/span[1]/select"
   ).select(2);
+
   cy.wait(1000);
   cy.xpath("/html[1]/body[1]/div[2]/div[1]/main[1]/button[1]/span[1]").click();
 }
-function crear_pagina_con_nombre_extenso(nombrePrueba) {
+function crear_pagina_con_nombre_extenso(nombrePrueba,ruta) {
   let titulo =
     "Tituloaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-  logIn();
-  cy.wait(5000);
+  let i=1;
+  logIn(i.toString(),ruta);
+  i+=1
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+
+
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/nav[1]/div[1]/section[1]/div[1]/ul[2]/li[2]/a[1]"
   ).click({ force: true });
   cy.wait(1000);
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
+
   // Click en el boton de page
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/div[1]/header[1]/section[1]/a[1]/span[1]"
   ).click({ force: true });
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
+
   tituloYDescricpion(titulo);
-  publicarPagina(titulo);
+  publicarPagina(i,ruta);
+  i+=1
   cy.wait(2000);
 
   //Ir a la seccion de paginas publicadas
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/main[1]/section[1]/div[1]/header[1]/section[1]/div[1]/div[1]/div[1]/span[1]"
   ).click();
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
+
   cy.wait(2000);
   cy.xpath("/html[1]/body[1]/div[1]/div[1]/ul[1]/li[3]").click();
   cy.wait(2000);
@@ -202,6 +264,8 @@ function crear_pagina_con_nombre_extenso(nombrePrueba) {
   ).click();
   cy.wait(2000);
   cy.xpath("/html[1]/body[1]/div[1]/div[1]/ul[1]/li[1]").click();
+  cy.screenshot(ruta+"/step"+i.toString(),{overwrite: true, capture: 'fullPage'})
+  i+=1
 
   verificarWidth();
 }
@@ -248,23 +312,24 @@ function tituloYDescricpion(titulo) {
   cy.wait(1000);
 }
 
-function publicarPagina() {
-  // Click en publish
-  cy.xpath(
-    "/html[1]/body[1]/div[2]/div[1]/main[1]/div[1]/section[1]/header[1]/section[1]/div[2]/div[1]/span[1]"
-  ).click();
+  function publicarPagina(i,ruta) {
+    // Click en publish
+    cy.xpath(
+      "/html[1]/body[1]/div[2]/div[1]/main[1]/div[1]/section[1]/header[1]/section[1]/div[2]/div[1]/span[1]"
+    ).click();
+    cy.screenshot(ruta+"/step"+i,{overwrite: true, capture: 'fullPage'})
 
-  cy.xpath(
-    "/html[1]/body[1]/div[1]/div[1]/footer[1]/button[2]/span[1]"
-  ).click();
-
-  cy.wait(2000);
-  // Volver a la seccion de pages
-  cy.xpath(
-    "/html[1]/body[1]/div[2]/div[1]/main[1]/div[1]/section[1]/header[1]/div[1]/div[1]/a[1]/span[1]"
-  ).click();
-  cy.wait(2000);
-}
+    cy.xpath(
+      "/html[1]/body[1]/div[1]/div[1]/footer[1]/button[2]/span[1]"
+    ).click();
+   
+    cy.wait(2000);
+    // Volver a la seccion de pages
+    cy.xpath(
+      "/html[1]/body[1]/div[2]/div[1]/main[1]/div[1]/section[1]/header[1]/div[1]/div[1]/a[1]/span[1]"
+    ).click();
+    cy.wait(2000);
+  }
 function exit() {
   cy.xpath(
     "/html[1]/body[1]/div[2]/div[1]/nav[1]/div[1]/section[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/*[name()='svg'][1]/*[name()='path'][1]"
